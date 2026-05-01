7649f4f17431d251dbf89c765f79497d31710637
Janavi-raj <janavih.cse@skit.org.in>
2026-05-01 18:18:33 +0530

    connect frontend to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ 
-  baseURL: 'https://team-task-manager-production-37eb.up.railway.app/api' 
+const API = axios.create({
+  baseURL: "[team-task-manager-production-37eb.up.railway.app](https://team-task-manager-production-37eb.up.railway.app/api)",
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
