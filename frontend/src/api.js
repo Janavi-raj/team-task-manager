2e19639b6eb7f1f5d5ececc9d49e124f791c4b1e
Janavi-raj <janavih.cse@skit.org.in>
2026-05-01 18:33:13 +0530

    Fixed API baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "[team-task-manager-production-37eb.up.railway.app](https://team-task-manager-production-37eb.up.railway.app/api)",
+  baseURL: "https://team-task-manager-production-37eb.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
